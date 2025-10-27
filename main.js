6f9f45bf0dc12f1620b3ab373e90a8820dec0d96
miukumauku123 <matias.kola@centria.fi>
2025-10-27 12:57:19 +0200

    better recipe system

---
+++ main.js
@@ -0,0 +1,39 @@
+const recipes = [
+    {
+        title: "Example food",
+        author: "Example Man",
+        description: "Example description",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["tag1", "tag2", "tag3"],
+        image: "img/8-3-large.jpg"
+    }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementsByClassName("recipe-area")[0];
+
+    recipes.forEach(recipe => {
+        const recipeBox = document.createElement("div");
+        recipeBox.classList.add("recipe-box");
+
+        const recipeTitle = document.createElement("h2");
+        recipeTitle.textContent = recipe.title;
+
+        const recipeCreator = document.createElement("p");
+        recipeCreator.textContent = recipe.author;
+
+        const recipeImage = document.createElement("img");
+        recipeImage.src = recipe.image;
+
+        const recipeTags = document.createElement("div");
+        recipeTags.textContent = recipe.tags.join(", ");
+
+        recipeBox.appendChild(recipeTitle);
+        recipeBox.appendChild(recipeCreator);
+        recipeBox.appendChild(recipeImage);
+        recipeBox.appendChild(recipeTags);
+
+        container.appendChild(recipeBox);
+    })
+});
+
