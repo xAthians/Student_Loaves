a67cde5718570467919607814e965c332c8d8119
miukumauku123 <matias.kola@centria.fi>
2025-10-28 10:00:32 +0200

    recipes seperated by categories

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 const recipes = [
     {
         id: "example-food",
+        category: "breakfast",
         title: "Example food",
         author: "Example Man",
+        description: "Example description",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["tag1", "tag2", "tag3"],
+        image: "img/8-3-large.jpg"
+    },
+    {
+        id: "example-food2",
+        category: "lunch",
+        title: "Example lunch",
+        author: "Example Chef",
+        description: "Example description",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["tag1", "tag2", "tag3"],
+        image: "img/8-3-large.jpg"
+    },
+    {
+        id: "example-food3",
+        category: "snacks",
+        title: "Bread snack?",
+        author: "Example Chef",
+        description: "Example description",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["tag1", "tag2", "tag3"],
+        image: "img/8-3-large.jpg"
+    },
+    {
+        id: "example-food4",
+        category: "dinner",
+        title: "Baguette dinner",
+        author: "Gordon Ramsay",
+        description: "Example description",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["tag1", "tag2", "tag3"],
+        image: "img/8-3-large.jpg"
+    },
+    {
+        id: "example-food5",
+        category: "beforebed",
+        title: "Example food 5",
+        author: "Example Chef",
         description: "Example description",
         ingredients: ["ingredient 1", "ingredient 2"],
         tags: ["tag1", "tag2", "tag3"],
@@ -11,9 +52,10 @@ const recipes = [
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementsByClassName("recipe-area")[0];
-
     recipes.forEach(recipe => {
+        const container = document.querySelector(`#${recipe.category} .recipe-area`);
+        if (!container) return;
+
         const recipeBox = document.createElement("div");
         recipeBox.classList.add("recipe-box");
         recipeBox.id = recipe.id;
@@ -36,6 +78,6 @@ document.addEventListener("DOMContentLoaded", () => {
         recipeBox.appendChild(recipeTags);
 
         container.appendChild(recipeBox);
-    })
+    });
 });
 
