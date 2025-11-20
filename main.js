4a2807c6b94dfcda780673c2ca99dc6b15a530d1
miukumauku123 <matias.kola@centria.fi>
2025-11-20 10:51:18 +0200

    Big commit: all recipes, new tabs changed to work based on tags, minor fixes

--- main.js
+++ main.js
@@ -1,57 +1,242 @@
 recipes = [
     {
-        id: "example-food",
-        category: "breakfast",
-        title: "Example food",
-        author: "Example Man",
-        description: "Example description",
-        ingredients: ["ingredient 1", "ingredient 2"],
-        tags: ["tag1", "tag2", "tag3"],
-        image: "img/8-3-large.jpg"
+        id: "basic-bread",
+        title: "Basic bread recipe",
+        author: "Iza",
+        description: "The easiest bread recipe you'll find! Excellent for first time loafers ;)",
+        ingredients: [
+            "500g flour",
+            "2 tsp instant yeast",
+            "3 tbsp oliver oil",
+            "2 tsp salt",
+            "Glass of water"
+        ],
+        tags: ["Easy", "Vegan", "Bread", "From the scratch"],
+        image: "img/basic-bread.jpg"
     },
     {
-        id: "example-food2",
-        category: "lunch",
-        title: "Example lunch",
-        author: "Example Chef",
-        description: "Example description",
-        ingredients: ["ingredient 1", "ingredient 2"],
-        tags: ["tag1", "tag2", "tag3"],
-        image: "img/8-3-large.jpg"
+        id: "basic-chicken-soup",
+        title: "Basic chicken soup",
+        author: "Iza",
+        description: "When the weather starts to get bad and you feel the need for a quick immunity boost this is the perfect recipe for you!",
+        ingredients: [
+            "1 large onion",
+            "4 carrots",
+            "1 small celery root",
+            "3 parsley roots",
+            "1 leek",
+            "1 bunch of parsley",
+            "2 chicken breasts",
+            "5 tbsp olive oil (or other)",
+            "Around 5 bay leaves",
+            "2 tsp salt",
+            "5 allsprice berries",
+            "10 peppercorns"
+        ],
+        tags: ["Soup", "When you're homesick", "Healthy", "Dinner"],
+        image: "img/basic-chicken-soup.jpg"
     },
     {
-        id: "example-food3",
-        category: "snacks",
-        title: "Bread snack?",
-        author: "Example Chef",
-        description: "Example description",
+        id: "quick-comforting-noodles",
+        title: "Quick comforting noodles",
+        author: "Iza",
+        description: "When you feel like eating instant ramen, but you want to fix your diet at least a little bit.",
         ingredients: ["ingredient 1", "ingredient 2"],
-        tags: ["tag1", "tag2", "tag3"],
-        image: "img/8-3-large.jpg"
+        tags: ["Vegan", "Dinner"],
+        image: "img/quick-comforting-noodles.jpg"
     },
     {
-        id: "example-food4",
-        category: "dinner",
-        title: "Baguette dinner",
-        author: "Gordon Ramsay",
-        description: "Example description",
+        id: "quick-n-easy-pie",
+        title: "Quick and easy pie",
+        author: "Iza",
+        description: "High protein pie you can eat for breakfast or add to any other meal.",
         ingredients: ["ingredient 1", "ingredient 2"],
-        tags: ["tag1", "tag2", "tag3"],
-        image: "img/8-3-large.jpg"
+        tags: ["Vegetarian", "Healthy"],
+        image: "img/quick-n-easy-pie.jpg"
     },
     {
-        id: "example-food5",
-        category: "beforebed",
-        title: "Example food 5",
-        author: "Example Chef",
-        description: "Example description",
+        id: "fit-buns",
+        title: "Fit buns",
+        author: "Iza",
+        description: "Bread, but make it healthier",
         ingredients: ["ingredient 1", "ingredient 2"],
-        tags: ["tag1", "tag2", "tag3"],
-        image: "img/8-3-large.jpg"
+        tags: ["Vegetarian", "Healthy", "Bread", "Breakfast", "From the scratch"],
+        image: "img/fit-buns.jpg"
+    },
+    {
+        id: "egg-tortilla",
+        title: "Egg tortilla",
+        author: "Iza",
+        description: "Quick breakfast idea",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Healthy", "Breakfast"],
+        image: "img/egg-tortilla.jpg"
+    },
+    {
+        id: "pancakes",
+        title: "Pancakes",
+        author: "Iza",
+        description: "Just normal pancakes you can eat plain or add anything into (both sweet and savoury).",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Breakfast"],
+        image: "img/pancakes.jpg"
+    },
+    {
+        id: "zucchini-bread",
+        title: "Flatbread made from zucchini",
+        author: "Iza",
+        description: "Quick breakfast idea",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Healthy", "Breakfast", "Bread"],
+        image: "img/zucchini-bread.jpg"
+    },
+    {
+        id: "carrot-bread",
+        title: "Carrot bread",
+        author: "Iza",
+        description: "Flatbread made from carrots",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Healthy", "Breakfast", "Bread"],
+        image: "img/carrot-bread.jpeg"
+    },
+    {
+        id: "banana-bread",
+        title: "Banana bread",
+        author: "Iza",
+        description: "A very basic, but very delicious dessert recipe",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Sweets", "From the scratch"],
+        image: "img/banana-bread.jpg"
+    },
+    {
+        id: "basic-cake",
+        title: "Basic cake",
+        author: "Iza",
+        description: "For your friends birthday, for celebrating a passed exam or even just to make your day a bit better",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Sweets", "From the scratch"],
+        image: "img/basic-cake.jpg"
+    },
+    {
+        id: "basic-muffins",
+        title: "Basic muffins",
+        author: "Iza",
+        description: "For any occasion",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Sweets", "From the scratch"],
+        image: "img/basic-muffins.jpg"
+    },
+    {
+        id: "pad-thai",
+        title: "Pad thai",
+        author: "Iza",
+        description: "Quick dinner idea",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Dinner"],
+        image: "img/pad-thai.jpg"
+    },
+    {
+        id: "fried-rice",
+        title: "Fried rice",
+        author: "Iza",
+        description: "Quick dinner idea",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Dinner"],
+        image: "img/fried-rice.jpg"
+    },
+    {
+        id: "chocolate-chip-cookies",
+        title: "Chocolate chip cookies",
+        author: "Iza",
+        description: "For when you need that homely comfort",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Sweets", "From the scratch", "When you're homesick"],
+        image: "img/chocolate-chip-cookies.jpg"
+    },
+    {
+        id: "gingerbread-cookies",
+        title: "Gingerbread cookies",
+        author: "Iza",
+        description: "Homely comfort (Christmas edition!)",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Sweets", "From the scratch", "When you're homesick"],
+        image: "img/gingerbread-cookies.jpg"
+    },
+    {
+        id: "chicken-cutlets",
+        title: "Chicken cutlets",
+        author: "Iza",
+        description: "Dinner idea",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["When you're homesick", "Dinner"],
+        image: "img/chicken-cutlets.jpg"
+    },
+    {
+        id: "tofu-steaks",
+        title: "Tofu steaks",
+        author: "Iza",
+        description: "Dinner idea",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegan", "Dinner"],
+        image: "img/tofu-steaks.jpg"
+    },
+    {
+        id: "overnight-oats",
+        title: "Overnight oats",
+        author: "Iza",
+        description: "Quick breakfast meal prep",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegan", "Healthy", "Breakfast"],
+        image: "img/overnight-oats.jpg"
+    },
+    {
+        id: "extra-cheesy-toast",
+        title: "Extra cheesy toast",
+        author: "Iza",
+        description: "A simple trick on how to step up your toast game",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegetarian", "Breakfast"],
+        image: "img/extra-cheesy-toast.png"
+    },
+    {
+        id: "egg-paste",
+        title: "Egg paste",
+        author: "Iza",
+        description: "A simple homely spread",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Easy", "Vegetarian", "Breakfast", "When you're homesick", "From the scratch"],
+        image: "img/egg-paste.jpg"
+    },
+    {
+        id: "tuna-melt",
+        title: "Tuna melt",
+        author: "Iza",
+        description: "A simple toast, but feels fancier!",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Breakfast"],
+        image: "img/tuna-melt.jpg"
+    },
+    {
+        id: "mashed-potatoes",
+        title: "Mashed potatoes",
+        author: "Iza",
+        description: "A quick side/dinner idea",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["When you're homesick", "Vegetarian", "Dinner"],
+        image: "img/mashed-potatoes.jpeg"
+    },
+    {
+        id: "grilled-vegetables",
+        title: "Grilled vegetables",
+        author: "Iza",
+        description: "A trick on how to use up the veggies laying at the back of your drawer",
+        ingredients: ["ingredient 1", "ingredient 2"],
+        tags: ["Vegan", "Healthy", "Dinner"],
+        image: "img/grilled-vegetables.jpg"
     }
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
     recipes.forEach(recipe => {
         const container = document.querySelector(`#${recipe.category} .recipe-area`);
         if (!container) return;
@@ -78,6 +263,45 @@ document.addEventListener("DOMContentLoaded", () => {
         recipeBox.appendChild(recipeTags);
 
         container.appendChild(recipeBox);
+    });
+});
+*/
+document.addEventListener("DOMContentLoaded", () => {
+    const contentBoxes = document.querySelectorAll(".content-box");
+
+    recipes.forEach(recipe => {
+        contentBoxes.forEach(box => {
+            const boxTag = box.dataset.tag;
+
+            if (recipe.tags.includes(boxTag)) {
+
+                const container = box.querySelector(".recipe-area");
+                if (!container) return;
+
+                const recipeBox = document.createElement("div");
+                recipeBox.classList.add("recipe-box");
+                recipeBox.dataset.id = recipe.id;
+
+                const recipeTitle = document.createElement("h2");
+                recipeTitle.textContent = recipe.title;
+
+                const recipeCreator = document.createElement("p");
+                recipeCreator.textContent = recipe.author;
+
+                const recipeImage = document.createElement("img");
+                recipeImage.src = recipe.image;
+
+                const recipeTags = document.createElement("div");
+                recipeTags.textContent = recipe.tags.join(", ");
+
+                recipeBox.appendChild(recipeTitle);
+                recipeBox.appendChild(recipeCreator);
+                recipeBox.appendChild(recipeImage);
+                recipeBox.appendChild(recipeTags);
+
+                container.appendChild(recipeBox);
+            }
+        });
     });
 });
 
